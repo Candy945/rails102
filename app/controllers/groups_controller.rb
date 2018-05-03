7ce53a8eb0e6299f1b0c54e938d0ebfb0db9f30e
Candy945 <525387153@qq.com>
2018-05-03 18:40:15 +0800

    implement groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,4 +2,21 @@ class GroupsController < ApplicationController
   def index
     @groups = Group.all
   end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.create
+  end
+
+  def edit
+    @group = Group.edit
+  end
+
+  def delete
+    @group = Group.delete
+  end
+
 end
