96c419fb3a3b4c6efc8dbfa5c94d92acf9ed243e
Candy945 <525387153@qq.com>
2018-05-04 16:24:43 +0800

    implement groups#edit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -17,6 +17,10 @@ class GroupsController < ApplicationController
       redirect_to groups_path
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
   private
 
     def group_params
