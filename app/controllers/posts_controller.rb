4335b13d6fe3f24e1128a2485eaee794b715787c
Candy945 <525387153@qq.com>
2018-05-14 20:32:25 +0800

    use simple_format helper to fdecorate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,9 @@
 class PostsController < ApplicationController
   before_filter :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
+  before_action :find_group_and_check_permission, only:[:edit, :update, :destroy]
+
+
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -18,9 +22,35 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(post_params)
+
+  end
+
+  def update
+    @post = Post.find(post_params)
+    if @post.update(post_params)
+      redirect_to groups_path, notice:"Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    flash[:alert]="Post deleted"
+    redirect_to groups_path
+  end
+
    private
+
+   def find_group_and_check_permission
+     @group = Group.find(params[:group_id])
+     if current_user != @group.user
+       redirect_to root_path, alert: "You have no permission."
+     end
+   end
+
    def post_params
      params.require(:post).permit(:content)
    end
-
-end
